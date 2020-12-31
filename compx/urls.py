1fc5d73b5a5b76273e73d956be1ad0b371e452ba
Nickhil1737 <nikhilkumar2893@gmail.com>
2020-12-31 21:05:51 +0530

    added class view MyFormView

--- compx/urls.py
+++ compx/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
+from .views import MyFormView
 
 urlpatterns = [
-    path('', views.compHome, name='compHome'),
+    path('',MyFormView.as_view(), name='compHome'),
     path('compxdet/', views.compxdet, name='compxdet'),
 ]
--- compx/views.py
+++ compx/views.py
@@ -1,14 +1,51 @@
 # Create your views here.
+from django.views.generic import TemplateView
 import sys
 import csv
 from django.http import HttpResponseRedirect
+from django.views import View
 from django.shortcuts import render
-from django.urls import reverse
 from .forms import UploadFileForm
 from .models import ModelWithFileField
 import importlib
 sys.path.append('./compx')
 
+
+
+
+class MyFormView(View):
+    form_class = UploadFileForm
+    initial = {'key': 'value'}
+    template_name = 'compDetHome.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST,request.FILES)
+        if form.is_valid():
+            fname = form.cleaned_data['function_name']
+            savetofile(request.FILES['file'])
+            from . import newfind
+            filename="solve"
+            rescomp = "1"
+            lib = importlib.import_module(filename)
+            result = getattr(lib,fname)
+            rescomp = newfind.findcompx(result)
+            pfile = ""
+
+            with open('compx/solve.py') as filep:
+                pfile=filep.readlines()
+
+
+            instance = ModelWithFileField(fname_field=fname,code_field=pfile,complexity_field=rescomp)
+            instance.save()
+            # <process form cleaned data>
+            return HttpResponseRedirect('compxdet')
+
+        return render(request, self.template_name,{'form': form,'allinstances':ModelWithFileField.objects.all()})
+"""
 def compHome(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
@@ -34,14 +71,14 @@ def compHome(request):
     else:
         form = UploadFileForm()
     return render(request, 'compDetHome.html', {'form': form,'allinstances':ModelWithFileField.objects.all()})
-
+"""
 def savetofile(f):
     with open('compx/solve.py', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
 def compxdet(request):
-    instance1 = ModelWithFileField.objects.get(pk=12)
+    instances = ModelWithFileField.objects.all()
     context = {}
-    context['instance1'] = instance1
+    context['instances'] = instances
     return render(request, 'givecomp.html', context)
