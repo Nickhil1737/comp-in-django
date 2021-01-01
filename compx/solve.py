a5a4132c5d7cdc79dac0520977d37ef57faad676
Nickhil1737 <nikhilkumar2893@gmail.com>
2021-01-01 17:35:50 +0530

    added recently uploaded via db

--- compx/solve.py
+++ compx/solve.py
@@ -1,20 +1,20 @@
-# Python program for implementation of Linear Serach
-# Function needs an array and element to be found
-def LinearSearch(arr,x): 
-    n = len(arr)
-    flag = False
-    # Traverse through all array elements 
-    for i in range(n):
-        # Condition to check weather the element is found or not
-        
-        if arr[i] == x:
-            # Return True and the Set pos to index of Element in arr
-            return True
-    # Return -1 if no elemet found    
-    return False   
-        
-# TEST
-
-def solve(arr):
-    LinearSearch(arr,14095505)
-solve([1,1,1])
+import sys 
+  
+# Traverse through all array elements 
+def selectionSort(A):
+    for i in range(len(A)): 
+          
+        # Find the minimum element in remaining  
+        # unsorted array 
+        min_idx = i 
+        for j in range(i+1, len(A)): 
+            if A[min_idx] > A[j]: 
+                min_idx = j 
+                  
+        # Swap the found minimum element with  
+        # the first element         
+        A[i], A[min_idx] = A[min_idx], A[i] 
+  
+# test
+A = [64, 25, 12, 22, 11] 
+selectionSort(A)
